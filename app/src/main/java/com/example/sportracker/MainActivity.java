1f5ac9b21c9ce0f0bec0d930dc604dd5ca854ba7
tomporat247 <tomporat247@gmail.com>
2020-11-28 19:43:03 +0200

    add sign in activity

--- app/src/main/java/com/example/sportracker/MainActivity.java
+++ app/src/main/java/com/example/sportracker/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.sportracker;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
